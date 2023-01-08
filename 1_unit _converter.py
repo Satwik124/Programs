c94a3aa8db25960faa3da788fb73529076751557
Satwik124 <100058643+Satwik124@users.noreply.github.com>
2023-01-08 09:16:20 +0530

    Add files via upload

---
+++ 1_unit _converter.py
@@ -0,0 +1,10 @@
+#converting from centimeters to meters and centimeters to kilo meters
+s=float(input("please enter the value centimeter:"))
+meters=s/100
+kilometers=s/1000;
+print("%.2f Centimeters = %.2f Meters" %(s,meters))
+print("%.2f Centimeters = %.2f Kilometers" %(s,kilometers))
+#converting kilograms to grams
+grams=float(input("please enter in grams:"))
+kilograms=grams/1000;
+print("%.2f grams= %.2f kilograms" %(grams,kilograms))
